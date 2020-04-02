9fe86b9b5b7a29535df391b0bf86a94ed2680c34
pszdl <daniel.lea@nottingham.ac.uk>
2020-04-02 10:16:50 +0100

    added method to update_publications.py to get publications over a paginated response. Gets more than 25 but doesn't get all publications - unlike play_around.py

--- europepmc/management/commands/update_publications.py
+++ europepmc/management/commands/update_publications.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand, CommandError
 
 from europepmc.models import Biobank, Publication, Annotation, Tag
 
-import pyodbc
+# import pyodbc
 import requests
 
 
@@ -10,15 +10,43 @@ class Command(BaseCommand):
 
     help = 'Import publications from EBI endpoint'
 
+    # def handle(self, *args, **options):
+    #
+    #     endpoint = 'https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=ACK_FUND:"{}"&format=json'
+    #
+    #     for biobank in Biobank.objects.all():
+    #
+    #         r = requests.get(endpoint.format(biobank))
+    #
+    #         for publication in r.json()['resultList']['result']:
+    #             pubYear = publication['pubYear']
+    #             title = publication['title']
+    #             pid = publication['id']
+    #             source = publication['source']
+    #
+    #             try:
+    #                 doi = publication['doi']
+    #             except KeyError as e:
+    #                 doi = ''
+    #
+    #             Publication.objects.get_or_create(
+    #                 year=pubYear,
+    #                 title=title,
+    #                 biobank=biobank,
+    #                 pid=pid,
+    #                 source=source,
+    #                 doi=doi,
+    #             )
+
     def handle(self, *args, **options):
 
-        endpoint = 'https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=ACK_FUND:"{}"&format=json'
+        # endpoint = 'https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=ACK_FUND:"{}"&format=json'
 
         for biobank in Biobank.objects.all():
 
-            r = requests.get(endpoint.format(biobank))
+            publications = self.get_articles_from_pagination(biobank)
 
-            for publication in r.json()['resultList']['result']:
+            for publication in publications:
                 pubYear = publication['pubYear']
                 title = publication['title']
                 pid = publication['id']
@@ -37,3 +65,33 @@ class Command(BaseCommand):
                     source=source,
                     doi=doi,
                 )
+
+    def get_articles_from_pagination(self, biobank: str):
+        """
+        iterate over a paginated request to get all the articles
+        :return:
+        """
+
+        endpoint = 'https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=ACK_FUND:"{}"&format=json'
+
+        req = requests.get(endpoint.format(biobank))
+        req_json = req.json()
+        req.close()
+
+        next_cursor = req_json.get('nextCursorMark', '')
+        # hit_count = req_json.get('hitCount', 0)
+        results = req_json.get('resultList', {}).get('result', [])
+        print(req.url)
+
+        while next_cursor != '*' and next_cursor != '':
+            req2 = requests.get(endpoint, params={'cursorMark': next_cursor})
+            req2_json = req2.json()
+            req2.close()
+
+            next_cursor = req2_json.get('nextCursorMark', '')
+            articles = req2_json.get('resultList', {}).get('result', [])
+            if not articles:
+                break
+            results.extend(articles)
+
+        return results
